82a9eeeb2741eb5b74e4179a17877724ba748ea4
nicoleamills <74476897+nicoleamills@users.noreply.github.com>
2022-02-17 11:47:20 -0600

    Add files via upload

---
+++ JLetter.java
@@ -0,0 +1,27 @@
+/** Simple program that displays letter J as a block letter.
+*
+* Project 1 - JLetter
+* @author Nicole Mills
+* @version 5.24.21
+*/
+
+public class JLetter {
+/** 
+* Each line prints out the letters J A V A in order to create block letter.
+* @param args Command line arguments - not used.
+*/
+   public static void main(String[] args) {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA");
+   }
+
+ 
+}
---
+++ MyLifeGoals.java
@@ -0,0 +1,29 @@
+/** Simple program that prints out my life goals.
+*
+* Project 1 - MyLifeGoals
+* @author Nicole Mills
+* @version 5.24.21
+*/
+
+public class MyLifeGoals {
+/** 
+* Prints short-term, medium-term, and long-term life goals.
+* @param args Command line arguments - not used.
+*/
+   public static void main(String[] args) {
+      System.out.println("Nicole Mills");
+      System.out.println();
+      System.out.println("My short-term life goals are to pass this class. "
+         + "Last semester, I made the mistake of trying to do too much at "
+         + "once. Now, I am only focusing on school and taking my time.");
+      System.out.println("My medium-term life goals are to move to a new city "
+         + "and work as a software engineer. I have an idea for a website "
+         + "or app that I would like to make, so I would love to "
+         + "gain the skills necessary in order to launch it.");
+      System.out.println("My long-term life goals are to invest in crypto, "
+         + "stocks, real estate, and retire early. I simply do not wish to "
+         + "work for my entire life haha.");
+   }
+
+
+}
